33c5267405c7d12a72a12ea1609b21b0c522db7b
ABHISHEKVALSAN <abhishekvalsan9999@gmail.com>
2018-10-08 20:16:46 +0530

    Update openAll.sh

--- openAll.sh
+++ openAll.sh
@@ -1,1 +1,1 @@
-firefox www.interviewbit.com www.hackerrank.com www.overleaf.com www.github.com  www.facebook.com www.linkedin.com www.twitter.com webmail.iitk.ac.in web.cse.iitk.ac.in webmail.cse.iitk.ac.in home.iitk.ac.in/~abhiavk http://placement.iitk.ac.in/dashboard/
+firefox www.interviewbit.com www.hackerrank.com www.overleaf.com www.github.com  www.facebook.com www.linkedin.com www.twitter.com webmail.iitk.ac.in web.cse.iitk.ac.in webmail.cse.iitk.ac.in home.iitk.ac.in/~abhiavk http://placement.iitk.ac.in/dashboard/ &
