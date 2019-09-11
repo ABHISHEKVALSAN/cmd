af1a4c860561d5eacc8ee599d4b7e6481a793446
abhishek_300067979 <abhishek.valsan@myntra.com>
2019-09-11 17:54:59 +0530

    f: plog.sh, wlog.sh --Making directories if not present.

--- plog.sh
+++ plog.sh
@@ -1,2 +1,3 @@
+mkdir -p ~/wlogs/$(date +"%Y")/$(date +"%h")
 echo "##########   $(date)  ##########" >> ~/plogs/$(date +"%Y")/$(date +"%h")/$(date +"%y")$(date +"%m")$(date +"%d")_$(date +"%a").txt
 nano ~/plogs/$(date +"%Y")/$(date +"%h")/$(date +"%y")$(date +"%m")$(date +"%d")_$(date +"%a").txt
